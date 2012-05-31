5e4496f92d5dcf7965f191bccdb894c02a2cddf5
alice <alice@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-05-31 21:04:35 +0000

    new modifs on NegBinomial normalisation
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ebs@11 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/src/LogBinNeg.cpp
+++ pkg/src/LogBinNeg.cpp
@@ -27,9 +27,9 @@ double LogBinNeg::operator()(int a, int b)
     return 0;
   int S = LesObs.SumInSegment(a,b);
   double L = LesObs.LogFactorialInSegment(a,b);
-  double P = LesObs.LogGammaPhiInSegment(a,b,phi);
+  //double P = LesObs.LogGammaPhiInSegment(a,b,phi);
   int n = b-a;
-  double Res = lgamma(beta+n*phi)+lgamma(S+alpha)-lgamma(a)-lgamma(b) +lgamma(a+b)-lgamma(beta+alpha+n*phi+S)-L+P;
+  double Res = lgamma(beta+n*phi)+lgamma(S+alpha)-lgamma(alpha)-lgamma(beta) +lgamma(alpha+beta)-lgamma(beta+alpha+n*phi+S)-L;
   return Res;
 }
 
